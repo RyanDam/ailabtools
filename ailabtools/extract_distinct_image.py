b654a7ec40faa8a810a22af84b8dd3c13bc9e9c6
quangnn2 <quangnn2@vng.com.vn>
2018-03-16 14:25:14 +0700

    add file_processer and extract_distinct_image

---
+++ ailabtools/extract_distinct_image.py
@@ -0,0 +1,92 @@
+import os
+import cv2
+import glob
+import imagehash
+import time
+import numpy as np
+from subprocess import call
+from PIL import Image
+
+def get_hash_images(im_dir):
+    if not os.path.isdir(im_dir):
+        raise Exception('{} is not directory!'.format(im_dir))
+    im_paths = glob.glob(im_dir + '/*.*', recursive=False)
+    hash_im = []
+    for path in im_paths:
+        im = Image.open(path)
+        hash_im.append((path, imagehash.average_hash(im)))
+    return hash_im
+
+
+def get_hash_video(video_dir, num_skip_frame):
+    video = cv2.VideoCapture(video_dir)
+    video_len = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) - 1 #last frame usually broken
+    if not video.isOpened():
+        raise Exception('Cant open video {}!'.format(video_dir))
+    
+    if num_skip_frame > video_len:
+        raise Exception('num_skip_frame({}) must less than total frame of video({})!'.format(num_skip_frame, 
+                                                                                             video_len))
+    
+    hash_frame = []
+    counter = 0
+    
+    for i in range(video_len):
+        rval, cur_frame = video.read()
+        if rval:
+            if counter % num_skip_frame == 0:
+                hash_frame.append((i, imagehash.average_hash(Image.fromarray(cur_frame))))
+            counter += 1
+
+    video.release()
+    return hash_frame
+    
+def save_frame_video(video_dir, frame_id, dest_dir):
+    video = cv2.VideoCapture(video_dir)
+    video_len = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) - 1 #last frame usually broken
+    if not video.isOpened():
+        raise Exception('Cant open video {}!'.format(video_dir))
+    
+    video_name = video_dir.split('/')[-1].split('.')[0]
+    for i in range(video_len):
+        _, cur_frame = video.read()
+        if i in frame_id:
+            image = Image.fromarray(cur_frame[:,:,::-1])
+            image_name = '{}_{}.png'.format(video_name, i)
+            image.save(os.path.join(dest_dir, image_name))
+    
+def extract_distinct_hashes(hashes, ration_remain):
+    if ration_remain > 1 or ration_remain < 0:
+        raise Exception('ration_remain must in range [0;1]')
+    
+    num_remain = ration_remain * len(hashes)
+    result = None
+    
+    max_distant = 64
+    for threshold in reversed(range(max_distant + 1)):
+        temp_res = [hashes[0]]
+        for _hash in hashes:
+            distant = max_distant
+            for res_item in temp_res:
+                distant = min(distant, _hash[1] - res_item[1])
+            if distant > threshold:
+                temp_res.append(_hash)
+        if len(temp_res) >= num_remain:
+            result = temp_res
+            break
+    return [_id for _id, _ in result]
+        
+    
+def extract_distinct_video(video_dir, dest_dir, num_skip_frame = 20, ration_remain = 0.1):
+    hashes = get_hash_video(video_dir, num_skip_frame)
+    image_id = extract_distinct_hashes(hashes, ration_remain)
+    save_frame_video(video_dir, image_id, dest_dir)
+    
+
+def extract_distinct_image(image_dir, dest_dir, ration_remain = 0.1):
+    hashes = get_hash_images(image_dir)
+    image_id = extract_distinct_hashes(hashes, ration_remain)
+    for path in image_id:
+        image_name = path.split('/')[-1]
+        new_path = os.path.join(dest_dir, image_name)
+        call(['scp', path, new_path])
---
+++ ailabtools/file_processer.py
@@ -0,0 +1,125 @@
+import os
+import sys
+import glob
+import magic
+import hashlib
+from subprocess import call
+from PIL import Image
+from send2trash import send2trash
+
+def __hashfile(path, blocksize = 65536):
+    afile = open(path, 'rb')
+    hasher = hashlib.md5()
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+    return hasher.hexdigest()
+
+def __change_namge(cur_name, new_name):
+    call(['mv', cur_name, new_name])
+    print('from', cur_name)
+    print('to', new_name)
+    print()
+
+def adj_extension(_dir):
+    if not os.path.isdir(_dir):
+        raise Exception('{} is not a directory!'.format(_dir))
+
+    #get path of all image
+    image_paths = glob.glob(_dir + '/**/*', recursive=True)
+    
+    num_img = 0
+    num_file_is_not_image = 0
+    num_img_add_extension = 0
+    num_img_change_extension = 0
+
+    for image_path in image_paths:
+        if os.path.isdir(image_path):
+            continue
+        num_img += 1
+        file_type = magic.from_file(path, mime=True).split('/')[0]
+        if file_type != 'image':
+            num_file_is_not_image += 1
+            send2trash(image_path)
+            print('Sent to trash:', image_path)
+            continue
+
+        real_ex = magic.from_file(path, mime=True).split('/')[-1]
+        im_name_splited = image_path.split('.')
+        
+        
+        #case: non-extensions
+        if len(im_name_splited) == 1:
+            num_img_add_extension += 1
+            im_name_splited.append(real_ex)
+            __change_namge(image_path, '.'.join(im_name_splited))
+            
+        #case: wrong-extensions
+        elif im_name_splited[-1] != real_ex:
+            num_img_change_extension += 1
+            im_name_splited[-1] = real_ex
+            __change_namge(image_path, '.'.join(im_name_splited))
+
+    print('Number of image:', num_img)
+    print('Number of file is not image (sent to trash):', num_img_not_in_list_extensions)
+    print('Number of file added extension:', num_img_add_extension)
+    print('Number of file changed extension:', num_img_change_extension)
+    print('Number of images remain:', num_img - num_img_not_in_list_extensions)
+
+
+
+def rm_duplicate(_dir):
+    if not os.path.isdir(_dir):
+        raise Exception('{} is not a directory!'.format(_dir))
+
+    #get path of all image
+    image_paths = glob.glob(_dir + '/**/*.*', recursive=True)
+    
+    num_duplicate = 0
+    #dictionary to save origin images by hash
+    origins = {}
+    
+    for image_path in image_paths:        
+        file_hash = __hashfile(image_path)
+        if file_hash in origins:
+            num_duplicate += 1
+            print('Move to trash:', image_path)
+            print('Duplicate with:', origins[file_hash])
+            print()
+            send2trash(image_path)
+        else:
+            origins[file_hash] = image_path
+            
+    #report
+    print('Total images:', len(image_paths))
+    print('Number of images duplicate:', num_duplicate)
+    print('Number of images remain:', len(image_paths) - num_duplicate)
+
+def rm_unreadable(_dir):
+
+    if not os.path.isdir(_dir):
+        raise Exception('{} is not a directory!'.format(_dir))
+
+    #get path of all image
+    image_paths = glob.glob(_dir + '/**/*.*', recursive=True)
+    
+    num_PIL_cant_open = 0
+    #dictionary to save origin images by hash
+    origins = {}
+    
+    for image_path in image_paths:
+        try:
+            Image.open(image_path)
+        except:
+            num_PIL_cant_open += 1
+            send2trash(image_path)
+            print('PIL cant open: ', image_path)
+            print()
+            continue
+            
+    #report
+    print('Total file:', len(image_paths))
+    print('Number of file PIL cant open:', num_PIL_cant_open)
+    print('Number of images remain:', len(image_paths) - num_PIL_cant_open)
