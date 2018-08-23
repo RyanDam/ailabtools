84262a0dd575204055f1b49cd14932b28274c041
quangnn2 <quangnn2@vng.com.vn>
2018-08-23 16:33:28 +0700

    update file_processor multi process

--- ailabtools/file_processer.py
+++ ailabtools/file_processer.py
@@ -1,11 +1,12 @@
 import os
 import sys
-import glob
+from glob import glob
 import magic
 import hashlib
 from subprocess import call
 from PIL import Image
 from send2trash import send2trash
+from multiprocessing import Pool
 
 def __hashfile(path, blocksize = 65536):
     afile = open(path, 'rb')
@@ -19,109 +20,80 @@ def __hashfile(path, blocksize = 65536):
 
 def __change_namge(cur_name, new_name):
     call(['mv', cur_name, new_name])
-    print('from', cur_name)
-    print('to', new_name)
-    print()
-
-#Adjust file's extension
-def adj_extension(_dir):
-    if not os.path.isdir(_dir):
-        raise Exception('{} is not a directory!'.format(_dir))
-
-    #get path of all image
-    image_paths = glob.glob(_dir + '/**/*', recursive=True)
+    print('change name file {} to {}'.format(cur_name, new_name))
     
-    num_img = 0
-    num_file_is_not_image = 0
-    num_img_add_extension = 0
-    num_img_change_extension = 0
+def __adj_extension(path):
+    real_ex = magic.from_file(path, mime=True).split('/')[1]
+    cur_ex = path.split('.')[-1]
+    if cur_ex != real_ex:
+        __change_namge(path, '{}.{}'.format(path, real_ex))
+        
+def adj_extension(paths, num_worker=4):
+    """Adjust extension of files
+    wrong_name => wrong_name.true_extension
 
-    for image_path in image_paths:
-        if os.path.isdir(image_path):
-            continue
-        num_img += 1
-        file_type = magic.from_file(path, mime=True).split('/')[0]
-        if file_type != 'image':
-            num_file_is_not_image += 1
-            send2trash(image_path)
-            print('Sent to trash:', image_path)
-            continue
+    Parameters
+    ----------
+    paths : list
+        list of path
+    num_worker: int
+        number of worker
 
-        real_ex = magic.from_file(path, mime=True).split('/')[-1]
-        im_name_splited = image_path.split('.')
+    Returns
+    -------
+    None
+    """
+    with Pool(num_worker) as p:
+        p.map(__adj_extension, paths)
         
         
-        #case: non-extensions
-        if len(im_name_splited) == 1:
-            num_img_add_extension += 1
-            im_name_splited.append(real_ex)
-            __change_namge(image_path, '.'.join(im_name_splited))
-            
-        #case: wrong-extensions
-        elif im_name_splited[-1] != real_ex:
-            num_img_change_extension += 1
-            im_name_splited[-1] = real_ex
-            __change_namge(image_path, '.'.join(im_name_splited))
+def __rm_unreadable(path):
+    try:
+        Image.open(path)
+    except:
+        send2trash(path)
+        print('remove file: {}'.format(path))
+        
+def rm_unreadable(paths, num_worker=4):
+    """Remove file witch PIL.Image faile to read
 
-    print('Number of image:', num_img)
-    print('Number of file is not image (sent to trash):', num_img_not_in_list_extensions)
-    print('Number of file added extension:', num_img_add_extension)
-    print('Number of file changed extension:', num_img_change_extension)
-    print('Number of images remain:', num_img - num_img_not_in_list_extensions)
+    Parameters
+    ----------
+    paths : list
+        list of path
+    num_worker: int
+        number of worker
 
+    Returns
+    -------
+    None
+    """
+    with Pool(num_worker) as p:
+        p.map(__rm_unreadable, paths)
+        
+def rm_duplicate(paths, num_worker=4):
+    """Remove duplicate file
 
-#remove duplicate file (bit level)
-def rm_duplicate(_dir):
-    if not os.path.isdir(_dir):
-        raise Exception('{} is not a directory!'.format(_dir))
+    Parameters
+    ----------
+    paths : list
+        list of path
+    num_worker: int
+        number of worker
 
-    #get path of all image
-    image_paths = glob.glob(_dir + '/**/*.*', recursive=True)
+    Returns
+    -------
+    None
+    """
     
-    num_duplicate = 0
-    #dictionary to save origin images by hash
-    origins = {}
-    
-    for image_path in image_paths:        
-        file_hash = __hashfile(image_path)
-        if file_hash in origins:
-            num_duplicate += 1
-            print('Move to trash:', image_path)
-            print('Duplicate with:', origins[file_hash])
-            print()
-            send2trash(image_path)
+    with Pool(num_worker) as p:
+        hashes = p.map(__hashfile, paths)
+        
+    filted = {}
+    remove_list = []
+    for i in range(len(hashes)):
+        if hashes[i] not in filted:
+            filted[hashes[i]] = paths[i]
         else:
-            origins[file_hash] = image_path
-            
-    #report
-    print('Total images:', len(image_paths))
-    print('Number of images duplicate:', num_duplicate)
-    print('Number of images remain:', len(image_paths) - num_duplicate)
-
-#remove file which PIL can't read
-def rm_unreadable(_dir):
-
-    if not os.path.isdir(_dir):
-        raise Exception('{} is not a directory!'.format(_dir))
-
-    #get path of all image
-    image_paths = glob.glob(_dir + '/**/*.*', recursive=True)
-    
-    num_PIL_cant_open = 0
-    #dictionary to save origin images by hash
-    origins = {}
-    
-    for image_path in image_paths:
-        try:
-            Image.open(image_path)
-        except:
-            num_PIL_cant_open += 1
-            send2trash(image_path)
-            print('PIL cant open: ', image_path)
-            print()
-            continue
-            
-    #report
-    print('Total file:', len(image_paths))
-    print('Number of file PIL cant open:', num_PIL_cant_open)
-    print('Number of images remain:', len(image_paths) - num_PIL_cant_open)
+            send2trash(paths[i])
+            print('remove file: {}'.format(paths[i]))
