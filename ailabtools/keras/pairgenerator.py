58a35950fa25ed659fb73ab5e416061024455549
anhptn2 <anhptn2@vng.com.vn>
2018-11-12 10:08:03 +0700

    Update pairgenerator.py for keras 2.2.4

--- ailabtools/keras/pairgenerator.py
+++ ailabtools/keras/pairgenerator.py
@@ -1,8 +1,8 @@
 import os
 import keras
-import keras_preprocessing
-import keras_preprocessing.image as kimage
-from keras_preprocessing.image import ImageDataGenerator, Iterator, load_img, img_to_array
+import keras.preprocessing
+import keras.preprocessing.image as kimage
+from keras.preprocessing.image import ImageDataGenerator, Iterator, load_img, img_to_array
 import numpy as np
     
 def load_img_func(fname, target_size, data_format):
