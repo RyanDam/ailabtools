e31f1f3863c3aa1784c5ede23b5853a280feb4db
quangnn2 <quangnn2@vng.com.vn>
2019-12-23 11:45:23 +0700

    Update ailab_multiprocessing.py, add option to use pool of thread instead process

--- ailabtools/ailab_multiprocessing.py
+++ ailabtools/ailab_multiprocessing.py
@@ -1,7 +1,8 @@
 from multiprocessing import Pool, cpu_count
+from multiprocessing.pool import ThreadPool
 from tqdm import tqdm
 
-def pool_worker(target, inputs, num_worker=None, verbose=True):
+def pool_worker(target, inputs, pool_type = 'process', num_worker=None, verbose=True):
     """Run target function in multi-process
 
     Parameters
@@ -11,6 +12,9 @@ def pool_worker(target, inputs, num_worker=None, verbose=True):
     inputs: list
         list of argument of target function
     num_worker: int
+    pool_type: str
+        valid value: ['process', 'thread']
+        type of pool worker
     number of worker
     verbose: bool
         True: progress bar
@@ -20,13 +24,20 @@ def pool_worker(target, inputs, num_worker=None, verbose=True):
     -------
     list of output of func
     """
+    if pool_type == "process":
+        pool_use = Pool
+    elif pool_type == "thread":
+        pool_use = ThreadPool
+    else:
+        raise("pool_type varibile only accept value 'process' or 'thread'")
+    
     if num_worker is None:
         num_worker = cpu_count()
 
     if verbose:
-        with Pool(num_worker) as p:
+        with pool_use(num_worker) as p:
             res = list(tqdm(p.imap(target, inputs), total=len(inputs)))
     else:
-        with Pool(num_worker) as p:
+        with pool_use(num_worker) as p:
             res = p.map(target, inputs)
     return res
